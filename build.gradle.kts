3addb9cb96ce66a670bcc2b4430309c529fd7ff0
dnya0 <annay01794@gmail.com>
2025-11-04 10:20:19 +0900

    chore: update dependencies for Exposed and PostgreSQL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 val kotlin_version: String by project
 val logback_version: String by project
+val postgres_version: String by project
+val exposed_version: String by project
 
 plugins {
     kotlin("jvm") version "2.2.20"
@@ -22,4 +24,12 @@ dependencies {
     implementation("io.ktor:ktor-server-config-yaml")
     testImplementation("io.ktor:ktor-server-test-host")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    //ktor exposed
+    implementation("org.jetbrains.exposed:exposed-core:${exposed_version}")
+    implementation("org.jetbrains.exposed:exposed-jdbc:${exposed_version}")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
+
+    //postgres
+    implementation("org.postgresql:postgresql:${postgres_version}")
 }
